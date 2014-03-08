ff920859a9eec58faac0ada9eda269a3510d1712
PITOLIN Kevin <kevin.pitolin@me.com>
2014-03-08 18:28:38 +0100

    liens

--- ProjetIfRoutard/src/metier/modele/InfoPrincipale.java
+++ ProjetIfRoutard/src/metier/modele/InfoPrincipale.java
@@ -10,6 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 
 /**
@@ -29,6 +30,8 @@ public class InfoPrincipale {
     private String Transport;
     @ManyToOne
     private Voyage voyageAssocie;
+    @OneToOne
+    private Devis devisAssocie;
     
     
     
